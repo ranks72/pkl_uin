380cfae0fd3950da97ecca382570d739aa36d853
Alfani <alfaniaulia09@gmail.com>
2021-02-25 19:04:02 +0700

    second commit, add modal ini admin menu

--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -8,10 +8,18 @@ class Menu extends CI_Controller
         $data['title'] = 'Menu Management';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
 
-        $this->load->view('templates/header', $data);
-        $this->load->view('templates/sidebar', $data);
-        $this->load->view('templates/topbar', $data);
-        $this->load->view('menu/index', $data);
-        $this->load->view('templates/footer');
+        $data['menu'] = $this->db->get('user_menu')->result_array();
+
+        // rules
+        $this->form_validation->set_rules('menu', 'Menu', 'required');
+
+        // Menu MOdal tambah data
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('menu/index', $data);
+            $this->load->view('templates/footer');
+        }
     }
 }
